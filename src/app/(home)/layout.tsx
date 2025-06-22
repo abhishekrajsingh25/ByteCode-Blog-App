2eee754ab3105532673e1b6148c40d1198407681
Abhishek Raj Singh <abhishekrajsingh2509@gmail.com>
2025-06-22 16:47:25 +0530

    Blog Application

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -6,21 +6,39 @@ import React from "react";
 const layout = async ({ children }: { children: React.ReactNode }) => {
  
   const user = await currentUser();
-  if (!user) {
-    return null;
-  }
-  const loggedInUser = await prisma.user.findUnique({
-    where: { clerkUserId: user.id },
-  });
-  if (!loggedInUser) {
-    await prisma.user.create({
-      data: {
-        name: `${user.fullName} ${user.lastName}`,
-        clerkUserId: user.id,
-        email: user.emailAddresses[0].emailAddress,
-        imageUrl: user.imageUrl,
-      },
+  // if (!user) {
+  //   return null;
+  // }
+  // const loggedInUser = await prisma.user.findUnique({
+  //   where: { clerkUserId: user.id },
+  // });
+  
+  // if (!loggedInUser) {
+  //   await prisma.user.create({
+  //     data: {
+  //       name: `${user.fullName} ${user.lastName}`,
+  //       clerkUserId: user.id,
+  //       email: user.emailAddresses[0].emailAddress,
+  //       imageUrl: user.imageUrl,
+  //     },
+  //   });
+  // }
+  
+  if (user) {
+    const loggedInUser = await prisma.user.findUnique({
+      where: { clerkUserId: user.id },
     });
+
+    if (!loggedInUser) {
+      await prisma.user.create({
+        data: {
+          name: `${user.fullName} ${user.lastName ?? ""}`,
+          clerkUserId: user.id,
+          email: user.emailAddresses[0].emailAddress,
+          imageUrl: user.imageUrl,
+        },
+      });
+    }
   }
 
   return (
